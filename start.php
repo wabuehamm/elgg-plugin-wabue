ad7f9e6aca3b08798147aaf6d011b8f5c1944c6b
Dennis Ploeger <develop@dieploegers.de>
2018-02-28 21:25:11 +0100

    Menu fixes

--- start.php
+++ start.php
@@ -1,8 +1,22 @@
 <?php
 function wabue_init() {
+	// Register HypeUI CSS fixes
 	elgg_extend_view('elements/layout.css', 'css/hypefixes');
+
+	// Register E-Mail address to profile view
 	elgg_extend_view('profile/details', 'profile/email');
+
+	// Register search menu item fix
+	elgg_register_plugin_hook_handler('register', 'menu:topbar', 'fixSearchMenuItem', 999);
+}
+
+function fixSearchMenuItem($hook, $type, $value, $params) {
+	foreach ($value as $menu_item) {
+		if ($menu_item->getName() == 'search') {
+			$menu_item->setPriority(10);
+		}
+	}
+	return $value;
 }
 
 elgg_register_event_handler('init', 'system', 'wabue_init');
-
