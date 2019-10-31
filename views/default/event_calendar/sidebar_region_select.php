577f9f1eb751d2c71f8dd93c7c74a7fb66287841
Dennis Ploeger <develop@dieploegers.de>
2019-10-31 20:05:43 +0100

    fix: Missing variables for region select

--- views/default/event_calendar/sidebar_region_select.php
+++ views/default/event_calendar/sidebar_region_select.php
@@ -2,6 +2,8 @@
 
 $event_calendar_region_display = elgg_get_plugin_setting('region_display', 'event_calendar');
 if ($event_calendar_region_display == 'yes') {
+	$filter_context = $vars['filter'];
+	$url_start = "event_calendar/list/{$vars['start_date']}/{$vars['mode']}";
 	elgg_require_js('event_calendar/event_calendar');
 	$url_start .= "/$filter_context";
 	echo elgg_view('event_calendar/region_select', ['url_start' => $url_start, 'region' => $vars['region']]);
