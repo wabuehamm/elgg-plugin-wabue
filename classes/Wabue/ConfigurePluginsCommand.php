06766ebc09d7188481c7fafc47b13e88fa22728d
Dennis Ploeger <develop@dieploegers.de>
2019-11-03 23:49:53 +0100

    fix: Enable poll for forum

--- classes/Wabue/ConfigurePluginsCommand.php
+++ classes/Wabue/ConfigurePluginsCommand.php
@@ -10,9 +10,14 @@ class ConfigurePluginsCommand extends \Elgg\Cli\Command {
     }
 
     protected function command() {
+        # Enable view change for event calendar
+
         elgg_set_plugin_setting('allow_view_change', 'yes', 'event_calendar');
+
+        # Enable polls globally and for forum group
         elgg_set_plugin_setting('enable_site', 'yes', 'poll');
         elgg_set_plugin_setting('enable_group', 'yes', 'poll');
+        get_entity(45105)->enableTool('poll');
 
         return 0;
     }
