b1a844163c1df46633f2d2e138b887f3d81563f4
Dennis Ploeger <develop@dieploegers.de>
2023-01-03 14:14:20 +0100

    fix: Fixed paged header to support date spanning events

--- views/default/event_calendar/paged_item_view.php
+++ views/default/event_calendar/paged_item_view.php
@@ -8,6 +8,18 @@ if (is_numeric($event->start_time)) {
 
 $date_bit = event_calendar_get_formatted_date($event->start_date);
 
+$end_time_bit = '';
+if (is_numeric($event->end_time)) {
+    $end_time_bit = event_calendar_convert_time($event->end_time);
+}
+
+$end_date_bit = event_calendar_get_formatted_date($event->end_date);
+
+$timespan = $date_bit . ' ' . $time_bit . ' - ' . $end_date_bit . ' ' . $end_time_bit;
+if ($date_bit == $end_date_bit) {
+    $timespan = $date_bit . ' ' . $time_bit . ' - ' . $end_time_bit;
+}
+
 if (event_calendar_has_personal_event($event->guid, elgg_get_logged_in_user_guid())) {
 	$calendar_bit = 'checked = "checked"';
 } else {
@@ -15,8 +27,7 @@ if (event_calendar_has_personal_event($event->guid, elgg_get_logged_in_user_guid
 }
 
 $info = '<tr>';
-$info .= '<td class="event_calendar_paged_date">' . $date_bit . '</td>';
-$info .= '<td class="event_calendar_paged_time">' . $time_bit . '</td>';
+$info .= '<td class="event_calendar_paged_time" style="width: 13em">' . $timespan . '</td>';
 $info .= '<td class="event_calendar_paged_title"><a href="' . $event->getUrl() . '">' . $event->title . '</a></td>';
 $info .= '<td class="event_calendar_paged_venue">' . $event->venue . '</td>';
 $info .= '<td class="event_calendar_paged_region">' . $event->region . '</td>';
